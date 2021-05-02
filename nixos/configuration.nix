1055d731bc3093498b8580f6d0ae7d519466b9b3
Lescurel <louis.klein7@gmail.com>
2021-05-02 18:15:14 +0200

    included home-manager into configuration.nix
    this will upgrade automatically home-manager packages with nixos-rebuild

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -11,27 +11,50 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./rice.nix 
+      <home-manager/nixos>
       ./zsh.nix
       ./direnv.nix
+      ./users/beren.nix
     ];
 
 
   # Use nvida non free packages
   hardware.nvidiaOptimus.disable = true;
-  hardware.opengl.extraPackages = [ pkgs.linuxPackages.nvidia_x11.out ];
-  hardware.opengl.extraPackages32 = [ pkgs.linuxPackages.nvidia_x11.lib32 ];
+  # hardware.opengl.extraPackages = [ pkgs.linuxPackages.nvidia_x11.out ];
+  # hardware.opengl.extraPackages32 = [ pkgs.linuxPackages.nvidia_x11.lib32 ];
   # hardware.opengl.extraPackages32 = [ pkgs_i686.linuxPackages.nvidia_x11.out ];
   
   hardware.opengl.driSupport32Bit = true;
   hardware.pulseaudio.support32Bit = true;
+
+  hardware.bluetooth = {
+    enable = true;
+    settings = {
+      Policy = {
+        AutoEnable = false;
+      };
+    };
+  };
  
   # Use the systemd-boot EFI boot loader.
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.plymouth.enable = true;
 
-  networking.hostName = "beleriand"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking = {
+    hostName = "beleriand"; # Define your hostname.
+    wireless.enable = true;
+    firewall = {
+      enable = true;
+      allowedTCPPortRanges = [
+        {from = 1716; to = 1764;}
+      ];
+      allowedUDPPortRanges = [
+        {from = 1716; to = 1764;}
+      ];
+    };
+  }; 
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -49,36 +72,27 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; 
   # overriding a package
-  let 
-    RStudio-with-my-packages = rstudioWrapper.override{ packages = with rPackages; [ ggplot2 dplyr xts ]; };
-  in [
-    # hardware stuff
-    libfprint
+  [ 
     # basic utils
     wget
-    vim
-    # (import ./vim/my_vim.nix { inherit vim_configurable vimUtils vimPlugins stdenv fetchgit;})
-    #(import ./vim/another_vim.nix)
     git
     # DE
     nordic-polar
-    bspwm
-    sxhkd
-    xorg.xbacklight
-    rofi
-    rxvt_unicode
-    kitty
-    polybar
+    gnome3.gnome-tweaks
+    gnomeExtensions.gsconnect
     neofetch
-    feh
+
     htop
     mpv
-    arandr
+    vlc
+    ffmpeg
+    handbrake
+    youtube-dl
     zip
     unzip
     scrot
     neofetch
-    udiskie ntfs3g
+    ntfs3g
     ranger
     zathura
     bat
@@ -87,7 +101,8 @@
     # manage the home
     home-manager
     # web
-    firefox
+    firefox-wayland
+    chromium 
     transmission-gtk
     
     zsh
@@ -95,13 +110,10 @@
     # RStudio-with-my-packages
  
     # python stuff
-    conda
-    pypi2nix
-    # (python36.withPackages(ps: with ps; [ numpy toolz tensorflow Keras tensorflow-tensorboard]))
 
     # VMs and containers
     docker
-    nixops
+    # nixops # python2.7 CVE
     
     # security
     pass
@@ -110,19 +122,29 @@
     gimp
     vlc
     libreoffice
-    vscode
+    vscodium
     tdesktop
+    calibre
+    zoom-us
+    skype 
+    element-desktop
 
     # games
-    # steam
+    # discord 
+    steam
+    steam-run
+    dosbox
     wine
     lutris
+
+    # dev
+    pgmodeler
  
-    # Computing
-    # cudatoolkit
-    #  cudnn
 
   ];
+
+
+  # };
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -134,20 +156,11 @@
   virtualisation.virtualbox.host.enable = true;  
   users.extraGroups.vbxusers.members = [ "beren" ];
   # acpid
-  services.acpid = {
-    enable = true;
-    lidEventCommands = ''
-      export LID_STATE=$(awk '{print$NF}' /proc/acpi/button/lid/LID0/state)
-      if [[ $LID_TATE == "closed" ]]; then
-        ${pkgs.lightdm}/bin/dm-tool lock
-      fi
-    '';
-  };
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedTCPPorts = [ 64738 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
@@ -159,85 +172,120 @@
 
   # Enable sound.
   sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio = {
+    enable = true;
+    package = pkgs.pulseaudioFull;
+  };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.layout = "us";
-  services.xserver.xkbOptions = "compose:ralt";
-  # Enable touchpad support.
-  services.xserver.libinput.enable = true;
-
-  # We try to fix the tearing
-  services.xserver.videoDrivers = [ "intel" ];
-  services.xserver.deviceSection = ''
-    Option "DRI" "2"
-    Option "TearFree" "true"
-  '';
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    xkbOptions = "compose:ralt";
+    libinput.enable = true;
+    videoDrivers = ["intel"];
+    deviceSection = ''
+      Option "DRI" "2"
+      Option "TearFree" "true"
+    '';
+    displayManager.gdm = {
+      enable = true;
+      wayland = true;
+    };
+    desktopManager.gnome3.enable = true;
+  };
   
+  # to enable android phones
+  services.udev.packages = [ pkgs.android-udev-rules ];
+  programs.adb.enable = true;
+
+  # to login with fingerprint
+  # services.fprintd.enable = true;
+  # security.polkit = {
+  #  enable = true;
+  #  extraConfig = ''
+  #    polkit.addRule(function (action, subject) {
+  #      if (action.id == "net.reactivated.fprint.device.enroll") {
+  #        return subject.user == "root" ? polkit.Result.YES : polkit.result.NO
+  #     }
+  #  })
+  #   '';
+  # };
+
+  fonts.fonts = with pkgs;[
+    ubuntu_font_family
+    fira-code
+    powerline-fonts
+    noto-fonts-emoji
+    font-awesome-ttf
+    hack-font
+    siji
+    unifont
+    iosevka
+  ];
+
   # power management
   services.tlp = {
 	enable = true;
-	extraConfig = ''
-      TLP_ENABLE=1
-      TLP_DEFAULT_MODE=BAT
-      TLP_PERSISTENT_DEFAULT=1
-      DISK_IDLE_SECS_ON_AC=0
-      DISK_IDLE_SECS_ON_BAT=2
-      MAX_LOST_WORK_SECS_ON_AC=15
-      MAX_LOST_WORK_SECS_ON_BAT=60
-      CPU_SCALING_GOVERNOR_ON_AC=powersave
-      CPU_SCALING_GOVERNOR_ON_BAT=powersave
-      CPU_HWP_ON_AC=default
-      CPU_HWP_ON_BAT=balance_power
-      CPU_MAX_PERF_ON_AC=100
-      CPU_MIN_PERF_ON_BAT=0
-      CPU_MAX_PERF_ON_BAT=100
-      SCHED_POWERSAVE_ON_AC=1
-      SCHED_POWERSAVE_ON_BAT=1
-      NMI_WATCHDOG=0
-      ENERGY_PERF_POLICY_ON_AC=default
-      ENERGY_PERF_POLICY_ON_BAT=balance-power
-      DISK_DEVICES="sda sdb"
-      DISK_APM_LEVEL_ON_AC="254 254"
-      DISK_APM_LEVEL_ON_BAT="128 128"
-      SATA_LINKPWR_ON_AC="med_power_with_dipm max_performance"
-      SATA_LINKPWR_ON_BAT="med_power_with_dipm min_power"
-      AHCI_RUNTIME_PM_TIMEOUT=15
-      PCIE_ASPM_ON_AC=performance
-      PCIE_ASPM_ON_BAT=powersave
-      RADEON_POWER_PROFILE_ON_AC=high
-      RADEON_POWER_PROFILE_ON_BAT=low
-      RADEON_DPM_STATE_ON_AC=performance
-      RADEON_DPM_STATE_ON_BAT=battery
-      RADEON_DPM_PERF_LEVEL_ON_AC=auto
-      RADEON_DPM_PERF_LEVEL_ON_BAT=auto
-      WIFI_PWR_ON_AC=off
-      WIFI_PWR_ON_BAT=on
-      SOUND_POWER_SAVE_ON_AC=0
-      SOUND_POWER_SAVE_ON_BAT=0
-      SOUND_POWER_SAVE_CONTROLLER=Y
-      BAY_POWEROFF_ON_AC=0
-      BAY_POWEROFF_ON_BAT=0
-      BAY_DEVICE="sr0"
-      RUNTIME_PM_ON_AC=on
-      RUNTIME_PM_ON_BAT=auto
-      USB_AUTOSUSPEND=1
-      USB_BLACKLIST_BTUSB=0
-      USB_BLACKLIST_PHONE=0
-      USB_BLACKLIST_PRINTER=1
-      USB_BLACKLIST_WWAN=1
-      RESTORE_DEVICE_STATE_ON_STARTUP=0
-      DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE="bluetooth"
-    '';
+	settings = {
+        "TLP_ENABLE"=1;
+        "TLP_DEFAULT_MODE"="BAT";
+        "TLP_PERSISTENT_DEFAULT"=1;
+        "DISK_IDLE_SECS_ON_AC"=0;
+        "DISK_IDLE_SECS_ON_BAT"=2;
+        "MAX_LOST_WORK_SECS_ON_AC"=15;
+        "MAX_LOST_WORK_SECS_ON_BAT"=60;
+        "CPU_SCALING_GOVERNOR_ON_AC"="powersave";
+        "CPU_SCALING_GOVERNOR_ON_BAT"="powersave";
+        "CPU_HWP_ON_AC"="default";
+        "CPU_HWP_ON_BAT"="balance_power";
+        "CPU_MAX_PERF_ON_AC"=100;
+        "CPU_MIN_PERF_ON_BAT"=0;
+        "CPU_MAX_PERF_ON_BAT"=100;
+        "SCHED_POWERSAVE_ON_AC"=1;
+        "SCHED_POWERSAVE_ON_BAT"=1;
+        "NMI_WATCHDOG"=0;
+        "ENERGY_PERF_POLICY_ON_AC"="default";
+        "ENERGY_PERF_POLICY_ON_BAT"="balance-power";
+        "DISK_DEVICES"="sda sdb";
+        "DISK_APM_LEVEL_ON_AC"="254 254";
+        "DISK_APM_LEVEL_ON_BAT"="128 128";
+        "SATA_LINKPWR_ON_AC"="med_power_with_dipm max_performance";
+        "SATA_LINKPWR_ON_BAT"="med_power_with_dipm min_power";
+        "AHCI_RUNTIME_PM_TIMEOUT"=15;
+        "PCIE_ASPM_ON_AC"="performance";
+        "PCIE_ASPM_ON_BAT"="powersave";
+        "RADEON_POWER_PROFILE_ON_AC"="high";
+        "RADEON_POWER_PROFILE_ON_BAT"="low";
+        "RADEON_DPM_STATE_ON_AC"="performance";
+        "RADEON_DPM_STATE_ON_BAT"="battery";
+        "RADEON_DPM_PERF_LEVEL_ON_AC"="auto";
+        "RADEON_DPM_PERF_LEVEL_ON_BAT"="auto";
+        "WIFI_PWR_ON_AC"="off";
+        "WIFI_PWR_ON_BAT"="on";
+        "SOUND_POWER_SAVE_ON_AC"=0;
+        "SOUND_POWER_SAVE_ON_BAT"=0;
+        "SOUND_POWER_SAVE_CONTROLLER"="Y";
+        "BAY_POWEROFF_ON_AC"=0;
+        "BAY_POWEROFF_ON_BAT"=0;
+        "BAY_DEVICE"="sr0";
+        "RUNTIME_PM_ON_AC"="on";
+        "RUNTIME_PM_ON_BAT"="auto";
+        "USB_AUTOSUSPEND"=1;
+        "USB_BLACKLIST_BTUSB"=0;
+        "USB_BLACKLIST_PHONE"=0;
+        "USB_BLACKLIST_PRINTER"=1;
+        "USB_BLACKLIST_WWAN"=1;
+        "RESTORE_DEVICE_STATE_ON_STARTUP"=0;
+    };
   };
   location = {
-    latitude = 59.43;
-    longitude = 17.83;
+    latitude = 46.20;
+    longitude = 6.14;
   };
   # redshift
   services.redshift = {
-      enable = true;
+      enable = false;
       temperature = {
         day = 5500;
         night = 3500;
@@ -246,22 +294,6 @@
         day = "1.0";
         night = "0.7";
       };
-  };
-  # Enable the KDE Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
-  #services.xserver.desktopManager = {
-  #  default = "none";
-  #  xterm.enable = false;
-  #};
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.beren = {
-    isNormalUser = true;
-    home = "/home/beren";
-    shell = pkgs.zsh;
-    description = "Beren";
-    extraGroups = [ "wheel" "audio" "video" "docker" "beren"];
-    initialPassword = "test";
   };
 
   # This value determines the NixOS release with which your system is to be
---
+++ nixos/users/beren.nix
@@ -0,0 +1,81 @@
+{pkgs,...}:
+let colors = import ../colors.nix; in
+with colors;
+{
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.beren = {
+    isNormalUser = true;
+    home = "/home/beren";
+    shell = pkgs.zsh;
+    description = "Beren";
+    extraGroups = [ "wheel" "audio" "video" "docker" "beren" "adbusers" "plugdev" "input"];
+    initialPassword = "test";
+  };
+  home-manager.useUserPackages = true;
+  home-manager.useGlobalPkgs = true;
+  home-manager.users.beren = {
+    programs.git = {
+      package = pkgs.gitAndTools.gitFull;
+      enable = true;
+      userName = "Name";
+      userEmail = "Email";
+    };
+
+    programs.command-not-found.enable = true;
+
+    programs.neovim = {
+      enable = true;
+      extraConfig = builtins.readFile vim/vimrc;
+      vimAlias = true;
+      viAlias = true;
+      plugins = with pkgs.vimPlugins; [
+        sensible
+        vim-airline
+        The_NERD_tree # file system explorer
+        fugitive
+        vim-gitgutter # git
+        rust-vim
+        python-mode
+        vim-nix
+      ];
+      extraPython3Packages = (ps: with ps; [ python-language-server setuptools ]);
+    };
+
+    programs.zathura = {
+      enable = true;
+      options = {
+        # zathurarc-dark
+        notification-error-bg = "${dark}"; # base01  # seem not work
+        notification-error-fg = "${red}"; # red
+        notification-warning-bg = "${dark}"; # base01
+        notification-warning-fg = "${red}"; # red
+        notification-bg = "${dark}"; # base01
+        notification-fg = "${yellow}"; # yellow
+        completion-group-bg = "${mdark2}"; # base03
+        completion-group-fg = "${accent}"; # base0
+        completion-bg = "${dark}"; # base02
+        completion-fg = "${light}"; # base1
+        completion-highlight-bg = "${dark}"; # base01
+        completion-highlight-fg = "${mlight}"; # base2
+        index-bg = "${dark}"; # base02
+        index-fg = "${light}"; # base1
+        index-active-bg = "${dark}"; # base01
+        index-active-fg = "${accent}"; # base2
+        inputbar-bg = "${dark}"; # base01
+        inputbar-fg = "${accent}"; # base2
+        statusbar-bg = "${dark}"; # base02
+        statusbar-fg = "${light}"; # base1
+        highlight-color = "${green}"; # base00  # hightlight match when search keyword(vim's /)
+        highlight-active-color = "${blue}"; # blue
+        default-bg = "${dark}"; # base02
+        default-fg = "${light}"; # base1
+        recolor = true;
+        recolor-lightcolor = "${dark}"; # base02
+        recolor-darkcolor = "${light}"; # base1
+      };
+    };
+
+    services.lorri.enable = true;
+  };
+}
+
