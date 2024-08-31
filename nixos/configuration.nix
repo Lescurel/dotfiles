886dc13fddab66839fad52d499fa90dd328dfd18
Lescurel <louis.klein7@gmail.com>
2024-08-31 18:16:08 +0200

    updating config before cleaning up to handle 2 PCs

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -7,6 +7,13 @@
 {
   # allowing unfree packages :(
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.overlays = [
+    (self: super: {
+      fcitx-engines = pkgs.fcitx5;
+    })
+  ];
+  # Flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   imports =
     [ # Include the results of the hardware scan.
@@ -17,25 +24,31 @@
       ./users/beren.nix
     ];
 
+  # filesystem compression
+  fileSystems = {
+    "/".options = [ "subvol=root" "compress=zstd" ];
+    "/home".options = [ "subvol=home" "compress=zstd" ];
+    "/nix".options = [ "subvol=nix" "compress=zstd" "noatime" ];
+  };
+
 
   # Use nvida non free packages
   hardware.nvidiaOptimus.disable = true;
   # hardware.opengl.extraPackages = [ pkgs.linuxPackages.nvidia_x11.out ];
   # hardware.opengl.extraPackages32 = [ pkgs.linuxPackages.nvidia_x11.lib32 ];
   # hardware.opengl.extraPackages32 = [ pkgs_i686.linuxPackages.nvidia_x11.out ];
-  
+
   hardware.opengl.driSupport32Bit = true;
-  hardware.pulseaudio.support32Bit = true;
 
   hardware.bluetooth = {
     enable = true;
     settings = {
       Policy = {
-        AutoEnable = false;
+        AutoEnable = true;
       };
     };
   };
- 
+
   # Use the systemd-boot EFI boot loader.
 
   boot.loader.systemd-boot.enable = true;
@@ -66,19 +79,24 @@
   };
   console.keyMap = "us";
   # Set your time zone.
-  time.timeZone = "Europe/Paris";
+  time.timeZone = "Europe/Stockholm";
 
     # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; 
+  environment.systemPackages = with pkgs;
   # overriding a package
-  [ 
+  let
+    mySteam = steam.override {
+      extraPkgs = pkgs: with pkgs; [libpng];
+    };
+  in
+  [
     # basic utils
     wget
     git
     # DE
     nordic
-    gnome3.gnome-tweaks
+    gnome.gnome-tweaks
     gnomeExtensions.gsconnect
     neofetch
 
@@ -87,62 +105,72 @@
     vlc
     ffmpeg
     handbrake
-    youtube-dl
+    yt-dlp
     zip
     unzip
     scrot
     neofetch
     ntfs3g
     ranger
-    zathura
     bat
     niv
+    silver-searcher
 
     # manage the home
     home-manager
+
     # web
-    firefox-wayland
-    chromium 
+    (firefox.override { extraNativeMessagingHosts = [passff-host]; })
+    chromium
     transmission-gtk
-    
+
     zsh
-    
+
     # RStudio-with-my-packages
- 
+
     # python stuff
 
     # VMs and containers
     docker
     # nixops # python2.7 CVE
-    
-    # security
+
+    # password manager
     pass
- 
+    wl-clipboard
+    tomb
+
     # misc
     gimp
     vlc
     libreoffice
-    vscodium
     tdesktop
     calibre
     zoom-us
-    skype 
+    skypeforlinux
     element-desktop
+    # qgis insecure qtwebkit
+    cura
+    openscad
 
     # games
-    # discord 
-    steam
+    discord
+    mySteam
     steam-run
     dosbox
     wine
     lutris
+    openmw
 
     # dev
-    pgmodeler
- 
+    emacs
 
+    # audio
+    lmms
+    ardour
+    musescore
+
+    blender
   ];
-
 
   # };
   # Some programs need SUID wrappers, can be configured further or are
@@ -150,20 +178,20 @@
   # programs.mtr.enable = true;
   programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
   programs.vim.defaultEditor = true;
-  programs.browserpass.enable = true;
+  #programs.browserpass.enable = true;
+  #programs.firefox.nativeMessagingHosts.browserpass = true;
+
   # List services that you want to enable:
-  virtualisation.docker.enable = true;  
-  virtualisation.virtualbox.host.enable = true;  
+  virtualisation.docker.enable = true;
+  virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vbxusers.members = [ "beren" ];
   # acpid
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ 64738 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+
+  # TRIM for SSD
+  services.fstrim.enable = true;
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -171,10 +199,21 @@
   services.avahi.nssmdns = true;
 
   # Enable sound.
-  sound.enable = true;
+  # sound.enable = true;
   hardware.pulseaudio = {
+    enable = false;
+  #  package = pkgs.pulseaudioFull;
+  #  support32Bit = true;
+  };
+
+  security.rtkit.enable = true;
+  services.pipewire = {
     enable = true;
-    package = pkgs.pulseaudioFull;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    jack.enable = true;
   };
 
   # Enable the X11 windowing system.
@@ -194,10 +233,13 @@
     };
     desktopManager.gnome.enable = true;
   };
-  
+
   # to enable android phones
   services.udev.packages = [ pkgs.android-udev-rules ];
   programs.adb.enable = true;
+
+  # gnome extensions
+  services.gnome.gnome-settings-daemon.enable = true;
 
   # to login with fingerprint
   # services.fprintd.enable = true;
@@ -217,11 +259,10 @@
     fira-code
     powerline-fonts
     noto-fonts-emoji
-    font-awesome-ttf
+    font-awesome
     hack-font
     siji
     unifont
-    iosevka
   ];
 
   # power management
@@ -301,7 +342,6 @@
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "19.09"; # Did you read the comment?
+  system.stateVersion = "24.05"; # Did you read the comment?
 
 }
-
--- nixos/users/beren.nix
+++ nixos/users/beren.nix
@@ -18,12 +18,13 @@ with colors;
     home = "/home/beren";
     shell = pkgs.zsh;
     description = "Beren";
-    extraGroups = [ "wheel" "audio" "video" "docker" "beren" "adbusers" "plugdev" "input"];
+    extraGroups = [ "wheel" "audio" "video" "docker" "beren" "adbusers" "plugdev" "input" "network" "networkmanager"];
     initialPassword = "test";
   };
   home-manager.useUserPackages = true;
   home-manager.useGlobalPkgs = true;
   home-manager.users.beren = {
+    home.stateVersion = "24.05";
     programs.git = {
       package = pkgs.gitAndTools.gitFull;
       enable = true;
@@ -48,45 +49,13 @@ with colors;
         fugitive
         vim-gitgutter # git
         rust-vim
-        python-mode
+        # python-mode
         vim-nix
       ];
-      extraPython3Packages = (ps: with ps; [ python-language-server setuptools ]);
+      # extraPython3Packages = (ps: with ps; [ python-language-server setuptools ]);
     };
 
-    programs.zathura = {
-      enable = true;
-      options = {
-        # zathurarc-dark
-        notification-error-bg = "${dark}"; # base01  # seem not work
-        notification-error-fg = "${red}"; # red
-        notification-warning-bg = "${dark}"; # base01
-        notification-warning-fg = "${red}"; # red
-        notification-bg = "${dark}"; # base01
-        notification-fg = "${yellow}"; # yellow
-        completion-group-bg = "${mdark2}"; # base03
-        completion-group-fg = "${accent}"; # base0
-        completion-bg = "${dark}"; # base02
-        completion-fg = "${light}"; # base1
-        completion-highlight-bg = "${dark}"; # base01
-        completion-highlight-fg = "${mlight}"; # base2
-        index-bg = "${dark}"; # base02
-        index-fg = "${light}"; # base1
-        index-active-bg = "${dark}"; # base01
-        index-active-fg = "${accent}"; # base2
-        inputbar-bg = "${dark}"; # base01
-        inputbar-fg = "${accent}"; # base2
-        statusbar-bg = "${dark}"; # base02
-        statusbar-fg = "${light}"; # base1
-        highlight-color = "${green}"; # base00  # hightlight match when search keyword(vim's /)
-        highlight-active-color = "${blue}"; # blue
-        default-bg = "${dark}"; # base02
-        default-fg = "${light}"; # base1
-        recolor = true;
-        recolor-lightcolor = "${dark}"; # base02
-        recolor-darkcolor = "${light}"; # base1
-      };
-    };
+    programs.alacritty.enable=true;
 
     services.lorri.enable = true;
   };
